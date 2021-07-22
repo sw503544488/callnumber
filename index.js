a7706f5b9b72f96c573116e79116257dfe86dbf9
sw503544488 <503544488@qq.com>
2021-07-22 20:50:38 +0800

    叫号机

---
+++ index.js
@@ -0,0 +1,25 @@
+const divScreen =document.querySelector('#screen')
+const btnCreateNumber =document.querySelector('#creatNumber')
+const btnCallNumber =document.querySelector('#callNumber')
+const spanNewNumber =document.querySelector('#newNumber')
+const spanqueue =document.querySelector('#queue')
+
+let n=0;
+let queue=[];
+btnCreateNumber.onclick=()=>{
+  console.log('weqw')
+  n+=1;
+ // queue.push(n)
+  queue.push.call(queue,n)
+  spanNewNumber.innerText=n;
+  spanqueue.innerText=JSON.stringify(queue)
+}
+btnCallNumber.onclick=()=> {
+  const m=queue.shift.call(queue)
+  if (m===undefined) {
+   return ''
+  }else{
+  divScreen.innerText+='请 '+m+'号就餐'
+  spanqueue.innerText=JSON.stringify(queue)
+  }
+}
